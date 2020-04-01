8f70de220e78bcd022d80ed414b2476c3e402a0c
ADDANOM <58764507+ADDANOM@users.noreply.github.com>
2020-04-01 13:25:14 +0900

    player_move_complete

---
+++ Assets/02.Scripts/Ctrl_Check.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ctrl_Check : MonoBehaviour
+{
+    static public bool check_ctrl;
+
+    private float distance;
+    private Transform tr, tr1;
+    public GameObject other;
+
+    private void Awake()
+    {
+        tr = GetComponent<Transform>();
+        tr1 = other.GetComponent<Transform>();
+    }
+
+    private void Update()
+    {
+        Vector3 offset = tr.position - tr1.position;
+        distance = offset.sqrMagnitude;
+
+        if (distance < 0.01f)
+        check_ctrl = true;
+        else
+        check_ctrl = false;
+
+    }
+
+
+
+    // private void OnTriggerEnter(Collider other)
+    // {
+    //     if (other.gameObject.CompareTag("CONTROLLER"))
+    //     {
+    //         Debug.Log("true");
+    //         check_ctrl = true;
+    //     }
+    // }
+
+    // private void OnTriggerExit(Collider other)
+    // {
+
+    //     if (other.gameObject.CompareTag("CONTROLLER"))
+    //     {
+    //         Debug.Log("false");
+    //         check_ctrl = false;
+    //     }
+    
+    
+    // }  ===>>>> 움직일 시 false가 나오는 오류로 트리거/콜리더 검출은 폐기 
+
+
+
+
+
+}
--- Assets/02.Scripts/PlayerMove.cs
+++ Assets/02.Scripts/PlayerMove.cs
@@ -8,12 +8,24 @@ public class PlayerMove : MonoBehaviour
 {
     private Vector2 trackpad;
     private Transform tr;
+    private Rigidbody rg;
+
+    private Animator anim;
+    private float v, h;
+    private ParticleSystem Booster_1;
+    private ParticleSystem Booster_2;
+
+
     private Transform Controller_Tr, Controller_Tr2;
-    private Quaternion turn_angle;
 
+    static public bool check_ctrl;
 
-    private Vector3 moveDirection;
-    public float moveSpeed;
+    private float distance;
+    private Transform check_tr_1, check_tr_2;
+    public GameObject check_ctrl_left;
+    public GameObject check_ctrl_right;
+
+    public float forwardSpeed;
     public float moveSide;
     public float moveUp;
     public float speed = 50.0f;
@@ -24,12 +36,23 @@ public class PlayerMove : MonoBehaviour
 
 
 
-    void Start()
+    void Awake()
     {
-        moveSpeed = 0.0f;
+        v = 0.0f;
+        h = 0.0f;
+        forwardSpeed = 0.0f;
         tr = GetComponent<Transform>();
+        rg = GetComponent<Rigidbody>();
+
+        anim = transform.Find("FighterInterceptor").GetComponent<Animator>();
+        Booster_1 = transform.Find("FighterInterceptor").transform.Find("Booster_1").GetComponent<ParticleSystem>();
+        Booster_2 = transform.Find("FighterInterceptor").transform.Find("Booster_2").GetComponent<ParticleSystem>();
         Controller_Tr = transform.Find("[CameraRig]").transform.GetChild(1).transform.GetChild(1).GetComponent<Transform>();
         Controller_Tr2 = transform.Find("[CameraRig]").transform.GetChild(0).transform.GetChild(1).GetComponent<Transform>();
+
+        check_tr_1 = check_ctrl_left.GetComponent<Transform>();
+        check_tr_2 = check_ctrl_right.GetComponent<Transform>();
+
     }
 
     // Update is called once per frame
@@ -37,44 +60,110 @@ public class PlayerMove : MonoBehaviour
     {
         //moveDirection = Quaternion.AngleAxis(Player_Direction.transform.localRotation.eulerAngles.y, Vector3.up) * Vector3.forward;
 
+        ctrl();
+
         updateInput();
 
-        GetComponent<Rigidbody>().MovePosition(transform.position + transform.forward * moveSpeed + transform.right * moveSide * speed + transform.up * moveUp * speed);
-
+        if (check_ctrl == true)
+            rg.MovePosition(transform.position + transform.forward * forwardSpeed + transform.right * moveSide * speed + transform.up * moveUp * speed);
 
         //Debug.Log(Controller_Tr.eulerAngles.y); // left / right
-        Debug.Log(Controller_Tr.eulerAngles.y + " ; " + Controller_Tr2.eulerAngles.y); // up / down
+        //Debug.Log(Controller_Tr.eulerAngles.y + " ; " + Controller_Tr2.eulerAngles.y); // up / down
+
+
     }
 
+    private void ctrl() // 컨트롤러를 제대로 쥐었는가 판단 
+    {
+        Vector3 offset = check_tr_1.position - check_tr_2.position;
+        distance = offset.sqrMagnitude;
+
+        if (distance < 0.01f)
+            check_ctrl = true;
+        else
+            check_ctrl = false;
+
+    }
 
 
     private void updateInput()
     {
+
         trackpad = SteamVR_Actions._default.speedUp.GetAxis(rightHand);
-        moveSpeed = -1.0f * trackpad.x; // moving forward , backward
+        forwardSpeed = -1.0f * trackpad.x; // moving forward , backward
+
+        var main1 = Booster_1.main;
+        var main2 = Booster_2.main;
+        if (forwardSpeed < 0)
+        {
+            main1.startColor = new Color(1.0f, 0.51f, 0.31f, 0.0f);
+            main2.startColor = new Color(1.0f, 0.51f, 0.31f, 0.0f);
+        }
+        else if (forwardSpeed == 0)
+        {
+            main1.startColor = new Color(1.0f, 0.51f, 0.31f, 0.5f);
+            main2.startColor = new Color(1.0f, 0.51f, 0.31f, 0.5f);
+        }
+        else
+        {
+            main1.startColor = new Color(0.04f, 0.26f, 1.00f, 0.5f);
+            main2.startColor = new Color(0.04f, 0.26f, 1.00f, 0.5f);
+        }
 
         if (Controller_Tr.eulerAngles.x >= 300 && Controller_Tr.eulerAngles.x <= 350 && Controller_Tr.eulerAngles.y < 105) // left
         {
+            if (v >= -0.98f)
+                v -= 0.02f;
             moveSide = -1.0f * Time.deltaTime;
         }
         else if (Controller_Tr.eulerAngles.x >= 10 && Controller_Tr.eulerAngles.x <= 50 && Controller_Tr.eulerAngles.y > 75) // right
         {
+            if (v <= 0.98f)
+                v += 0.02f;
             moveSide = Time.deltaTime;
         }
         else
+        {
             moveSide = 0.0f;
+            if (v > 0.0f)
+                v -= 0.02f;
+            else if (v < 0.0f)
+                v += 0.02f;
+        }
+
 
 
         if (Controller_Tr.eulerAngles.z < 270) // up
         {
+            if (h <= 0.98f)
+                h += 0.02f;
             moveUp = Time.deltaTime;
         }
         else if (Controller_Tr.eulerAngles.z > 295) // down
         {
+            if (h >= -0.98f)
+                h -= 0.02f;
             moveUp = -1.0f * Time.deltaTime;
         }
         else
+        {
             moveUp = 0.0f;
+            if (h > 0.0f)
+                h -= 0.02f;
+            else if (h < 0.0f)
+                h += 0.02f;
+        }
+
+
+        anim.SetFloat("Player_Side", v);
+        anim.SetFloat("Player_Up", h);
+
+        // if (check_ctrl == false)
+        // {
+        //     anim.SetFloat("Player_Side", 0);
+        //     anim.SetFloat("Player_Up", 0);
+        // }
+
 
 
         // if (Controller_Tr.eulerAngles.y < 75) //..
@@ -98,8 +187,8 @@ public class PlayerMove : MonoBehaviour
         //     transform.rotation = Quaternion.RotateTowards(transform.rotation, turn_angle, Time.deltaTime * 40f);
         // }
 
-
-
-
     }
+
+
+
 }
