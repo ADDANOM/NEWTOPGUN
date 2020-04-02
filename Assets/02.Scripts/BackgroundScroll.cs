bdd50fdb84dc07cf2f4be76952b2971189e2287e
ADDANOM <58764507+ADDANOM@users.noreply.github.com>
2020-04-02 14:22:13 +0900

    zoneMaking

---
+++ Assets/02.Scripts/BackgroundScroll.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroll : MonoBehaviour
+{
+
+    Rigidbody rg;
+    float scrollSpeed;
+
+    void Start()
+    {
+        scrollSpeed = 2.0f;
+        rg = GetComponent<Rigidbody>();
+        
+    }
+
+
+    void Update()
+    {
+        if (transform.position.z > -4000.0f)
+        rg.MovePosition(transform.position + transform.forward*-1.0f*scrollSpeed);
+        if (transform.position.z <= -4000.0f)
+        rg.MovePosition(new Vector3(-3000,-700,4000));
+        
+    }
+}
---
+++ Assets/02.Scripts/PlayerWarning.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class PlayerWarning : MonoBehaviour
+{
+    public GameObject OutArea;
+    // GameObject outscreen;
+    // TextMeshPro _text;
+
+    GameObject warn_left;
+    GameObject warn_right;
+    GameObject warn_up;
+    GameObject warn_down;
+    GameObject warn_forward;
+    GameObject warn_backward;
+    Image _image;
+
+
+
+    void Start()
+    {
+        _image = OutArea.GetComponent<Image>();
+        // _text = outscreen.transform.GetChild(0).GetComponent<TextMeshPro>();
+        warn_left = OutArea.transform.GetChild(0).gameObject;
+        warn_right = OutArea.transform.GetChild(1).gameObject;
+        warn_down = OutArea.transform.GetChild(2).gameObject;
+        warn_up = OutArea.transform.GetChild(3).gameObject;
+        warn_forward = OutArea.transform.GetChild(4).gameObject;
+        warn_backward = OutArea.transform.GetChild(5).gameObject;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "AREA")
+        {
+            if (other.gameObject.name == "Zone_Up")
+            {
+                if (transform.position.y < 200.0f)
+                {
+                    Debug.Log("GO DOWN!");
+                    warn_down.SetActive(true);
+                    _image.color = new Color(0.2f, 0.0f, 1.0f, 0.25f);
+                }
+                else
+                {
+                    warn_down.SetActive(false);
+                    _image.color = new Color(0.2f, 0.0f, 1.0f, 0.0f);
+                }
+
+            }
+            else if (other.gameObject.name == "Zone_Down")
+            {
+                if (transform.position.y > -200.0f)
+                {
+                    Debug.Log("Go UP!");
+                    warn_up.SetActive(true);
+                    _image.color = new Color(0.2f, 0.0f, 1.0f, 0.25f);
+                }
+                else
+                {
+                    warn_up.SetActive(false);
+                    _image.color = new Color(0.2f, 0.0f, 1.0f, 0.0f);
+                }
+            }
+            else if (other.gameObject.name == "Zone_West")
+            {
+                if (transform.position.x > -300.0f)
+                {
+                    Debug.Log("Go RIGHT!");
+                    warn_right.SetActive(true);
+                    _image.color = new Color(0.2f, 0.0f, 1.0f, 0.25f);
+                }
+                else
+                {
+                    warn_right.SetActive(false);
+                    _image.color = new Color(0.2f, 0.0f, 1.0f, 0.0f);
+                }
+
+            }
+            else if (other.gameObject.name == "Zone_East")
+            {
+                if (transform.position.x < 300.0f)
+                {
+                    Debug.Log("Go LEFT!");
+                    warn_left.SetActive(true);
+                    _image.color = new Color(0.2f, 0.0f, 1.0f, 0.25f);
+                }
+                else
+                {
+                    warn_left.SetActive(false);
+                    _image.color = new Color(0.2f, 0.0f, 1.0f, 0.0f);
+                }
+
+            }
+            else if (other.gameObject.name == "Zone_North")
+            {
+                if (transform.position.z < 800.0f)
+                {
+                    Debug.Log("Go BACKWARD!");
+                    warn_backward.SetActive(true);
+                    _image.color = new Color(0.2f, 0.0f, 1.0f, 0.25f);
+                }
+                else
+                {
+                    warn_backward.SetActive(false);
+                    _image.color = new Color(0.2f, 0.0f, 1.0f, 0.0f);
+                }
+
+            }
+            else if (other.gameObject.name == "Zone_South")
+            {
+                if (transform.position.z > -200.0f)
+                {
+                    Debug.Log("Go FORWARD!");
+                    warn_forward.SetActive(true);
+                    _image.color = new Color(0.2f, 0.0f, 1.0f, 0.25f);
+                }
+                else
+                {
+                    warn_forward.SetActive(false);
+                    _image.color = new Color(0.2f, 0.0f, 1.0f, 0.0f);
+                }
+
+            }
+        }
+
+
+    }
+
+
+
+}
