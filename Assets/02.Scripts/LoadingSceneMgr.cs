ba99641970428c600b6a0ebf152196b16d00912b
Masill <59790064+Masill@users.noreply.github.com>
2020-04-09 18:03:29 +0900

    add loading / new Player

---
+++ Assets/02.Scripts/LoadingSceneMgr.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingSceneMgr : MonoBehaviour
+{
+    public static string nextScene;
+
+    [SerializeField]
+    Image prograssBar;
+
+    void Start()
+    {
+        StartCoroutine(LoadScene());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void LoadScene(string SceneName)
+    {
+        nextScene = SceneName;
+        SceneManager.LoadScene("Loading_Scene");
+    }
+
+    IEnumerator LoadScene()
+    {
+        yield return null;
+
+        AsyncOperation op = SceneManager.LoadSceneAsync(nextScene);
+        op.allowSceneActivation = false;
+
+        float timer = 0.0f;
+        while (!op.isDone)
+        {
+            yield return null;
+
+            timer += Time.deltaTime;
+
+            if(op.progress < 0.9f)
+            {
+                prograssBar.fillAmount = Mathf.Lerp(prograssBar.fillAmount, op.progress, timer);
+
+                if(prograssBar.fillAmount >= op.progress)
+                {
+                    timer = 0.0f;
+                }
+            }
+            else
+            {
+                prograssBar.fillAmount = Mathf.Lerp(prograssBar.fillAmount, 1f, timer);
+
+                if(prograssBar.fillAmount == 10.0f)
+                {
+                    op.allowSceneActivation = true;
+                    yield break;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/SelectCharactor.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectCharactor : MonoBehaviour
+{
+    bool bTurnLeft = false;
+    bool bTurnRight = false;
+    private Quaternion turn = Quaternion.identity;
+    // 정의
+    public static int charactorNum = 0;
+    int value = 0;
+
+    void Start()
+    {
+        turn.eulerAngles = new Vector3(0, value, 0);  // 각도 초기화
+        
+    }
+
+    void Update()
+    {
+        if (bTurnLeft)
+        {
+            charactorNum++;
+            if (charactorNum == 4)
+                charactorNum = 0;
+
+            value -= 90;
+            
+            bTurnLeft = false;
+        }
+        if (bTurnRight)
+        {
+            charactorNum--;
+            if (charactorNum == -1)
+                charactorNum = 3;
+
+            value += 90;
+            
+            bTurnRight = false;
+        }
+        turn.eulerAngles = new Vector3(0, value, 0);  // 각을 설정
+        
+        transform.rotation = Quaternion.Slerp(transform.rotation, turn, Time.deltaTime * 5.0f);  // 부드럽게 돌려준다.
+        
+    }
+
+    public void turnLeft()  // 다른 버튼 누를 때 까지 설정
+    {
+        bTurnLeft = true;
+        bTurnRight = false;
+    }
+
+    public void turnRight()
+    {
+        bTurnRight = true;
+        bTurnLeft = false;
+    }
+}
