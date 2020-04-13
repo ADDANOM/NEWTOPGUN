9dbccaf784ad79f85680ad004eaa8d0289c7a14d
Masill <59790064+Masill@users.noreply.github.com>
2020-04-13 16:57:15 +0900

    addd 123123

---
+++ Assets/02.Scripts/PlayList.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayList : MonoBehaviour
+{
+    AudioSource mySource;
+    public AudioClip[] myClip;
+
+    void Start()
+    {
+        mySource = GetComponent<AudioSource>();
+
+        mySource.clip = myClip[0];
+        mySource.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayLists();
+    }
+
+    void PlayLists()
+    {
+
+        if (!mySource.isPlaying)
+        {
+            mySource.clip = myClip[1];
+            mySource.Play();
+            mySource.loop = true;
+        }
+    }
+}
